15be20aa39059bab571bedd5a9379589e9c20af1
randomtestfive <randomtestfive@gmail.com>
2017-01-12 19:45:41 -0500

    Add NavX

--- src/org/redalert1741/steamworks/Robot.java
+++ src/org/redalert1741/steamworks/Robot.java
@@ -1,13 +1,26 @@
 package org.redalert1741.steamworks;
 
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.SPI.Port;
 
 public class Robot extends IterativeRobot
 {
+	private static AHRS navx;
+	
 	@Override
 	public void robotInit()
 	{
-
+		try
+		{
+			navx = new AHRS(Port.kMXP);
+        }
+		catch (RuntimeException ex )
+		{
+            DriverStation.reportError("Error instantiating navX MXP:  " + ex.getMessage(), true);
+        }
 	}
 
 	@Override
