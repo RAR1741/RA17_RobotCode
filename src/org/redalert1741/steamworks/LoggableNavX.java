4daa51f48b66260bbf6ff6ae9f9c30c97d6b08e4
randomtestfive <randomtestfive@gmail.com>
2017-01-16 18:36:02 -0500

    Setup NavX logging

---
+++ src/org/redalert1741/steamworks/LoggableNavX.java
@@ -0,0 +1,44 @@
+package org.redalert1741.steamworks;
+
+import org.redalert1741.robotBase.logging.DataLogger;
+import org.redalert1741.robotBase.logging.Loggable;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.SPI.Port;
+
+public class LoggableNavX extends AHRS implements Loggable
+{
+	public LoggableNavX(Port spi_port_id)
+	{
+		super(spi_port_id);
+	}
+
+	@Override
+	public void setupLogging(DataLogger logger)
+	{
+		logger.addAttribute("navx_Pitch");
+		logger.addAttribute("navx_Roll");
+		logger.addAttribute("navx_Yaw");
+		logger.addAttribute("navx_XAcc");
+		logger.addAttribute("navx_YAcc");
+		logger.addAttribute("navx_ZAcc");
+		logger.addAttribute("navx_Temp");
+		logger.addAttribute("navx_Bar");
+		logger.addAttribute("navx_Alt");
+	}
+
+	@Override
+	public void log(DataLogger logger)
+	{
+		logger.log("navx_Pitch", getPitch());
+		logger.log("navx_Roll", getRoll());
+		logger.log("navx_Yaw", getYaw());
+		logger.log("navx_XAcc", getRawAccelX());
+		logger.log("navx_YAcc", getRawAccelY());
+		logger.log("navx_ZAcc", getRawAccelZ());
+		logger.log("navx_Temp", getTempC());
+		logger.log("navx_Bar", getBarometricPressure());
+		logger.log("navx_Alt", getAltitude());
+	}
+}
--- src/org/redalert1741/steamworks/Robot.java
+++ src/org/redalert1741/steamworks/Robot.java
@@ -1,21 +1,26 @@
 package org.redalert1741.steamworks;
 
-import com.kauailabs.navx.frc.AHRS;
+import org.redalert1741.robotBase.logging.DataLogger;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.SPI.Port;
+import edu.wpi.first.wpilibj.Timer;
 
 public class Robot extends IterativeRobot
 {
-	private static AHRS navx;
+	private static LoggableNavX navx;
+	private static DataLogger logger;
+	private static Timer timer;
 	
 	@Override
 	public void robotInit()
 	{
+		timer = new Timer();
+		logger = new DataLogger();
 		try
 		{
-			navx = new AHRS(Port.kMXP);
+			navx = new LoggableNavX(Port.kMXP);
         }
 		catch (RuntimeException ex )
 		{
@@ -50,13 +55,19 @@ public class Robot extends IterativeRobot
 	@Override
 	public void testInit()
 	{
-		
+		timer.reset();
+		timer.start();
+		logger.open("/home/lvuser/navxTest.log");
+		logger.addLoggable(navx);
+		logger.setupLoggables();
+		logger.writeAttributes();
 	}
 
 	@Override
 	public void testPeriodic()
 	{
-		
+		logger.log();
+		logger.writeLine();
 	}
 }
 
