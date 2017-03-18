92b6f80fbe37681e9941aefccaefeb8b25f8928b
snydecor000 <snydecor000@students.centergrove.k12.in.us>
2017-03-18 13:57:44 -0400

    various different derps and stupids fixed

--- src/org/redalert1741/steamworks/Robot.java
+++ src/org/redalert1741/steamworks/Robot.java
@@ -1,6 +1,7 @@
 package org.redalert1741.steamworks;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import org.redalert1741.robotBase.logging.*;
@@ -23,6 +24,8 @@ public class Robot extends IterativeRobot
 	private static PowerDistributionPanel pdp;
 	private static Solenoid redLED;
 	private static Solenoid whiteLED;
+	private static Solenoid gearIN;
+	private static Solenoid gearOUT;
 	@SuppressWarnings("unused")
 	private String auto = "";
 	
@@ -67,6 +70,7 @@ public class Robot extends IterativeRobot
 //	private boolean configReload;
 	private JsonAutonomous auton;
 	private ScopeToggler scopeToggler;
+	ArrayList<String> memes;
 	
 	@Override
 	public void robotInit()
@@ -76,6 +80,8 @@ public class Robot extends IterativeRobot
 		pdp = new PowerDistributionPanel(20);
 		redLED = new Solenoid(0);
 		whiteLED = new Solenoid(1);
+		gearIN = new Solenoid(6);
+		gearOUT = new Solenoid(7);
 		
 		scopeToggler = new ScopeToggler(0,1);
 		Config.loadFromFile("/home/lvuser/config.txt");
@@ -137,8 +143,6 @@ public class Robot extends IterativeRobot
 		VisionThread.useAxisCamera();
 		VisionThread.enable();
 		VisionThread.setFilter(sf);
-
-		
 		ReloadConfig();
 	}
 //========================================================================================================
@@ -177,12 +181,27 @@ public class Robot extends IterativeRobot
 	; collect = false
 	; System.gc()
     ; }
+	
+	long avgMem = 0;
+	long avgC = 0;
+	
+	@Override
+	public void robotPeriodic()
+	{
+		super.robotPeriodic();
+		avgMem += Runtime.getRuntime().freeMemory();
+		avgC++;
+		//System.out.println("Heap Space: " + (double)avgMem/avgC);
+		//memes.add(memes.get(memes.size()-1).replace("m", "meeeeemmesess").replace("e", "memees"));
+	}
 
 	@Override
 	public void teleopPeriodic()
 	{
+		//VisionThread.disable();
 		System.out.println("HA: " + VisionThread.getHorizontalAngle());
 		System.out.println("Target: " + VisionThread.getBestRekt());
+		
 		if(driver.getYButton())
 		{
 			navx.reset();
@@ -254,9 +273,13 @@ public class Robot extends IterativeRobot
     	if(driver.getBumper(Hand.kLeft) || op.getBumper(Hand.kLeft))
     	{
     		gear.open();
+    		gearOUT.set(false);
+    		gearIN.set(true);
     	}
     	else
     	{
+    		gearOUT.set(true);
+    		gearIN.set(false);
     		gear.close();
     	}
     	///////////////////////////////////////////////////////////////////////////
