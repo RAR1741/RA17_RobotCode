87fa2c62c9db6b828379e0e2412dbff56f7199f8
randomtestfive <randomtestfive@gmail.com>
2017-03-18 13:16:14 -0400

    Create AutoInstructionConsumer interface

---
+++ src/org/redalert1741/steamworks/autonomous/AutoInstructionConsumer.java
@@ -0,0 +1,8 @@
+package org.redalert1741.steamworks.autonomous;
+
+public interface AutoInstructionConsumer
+{
+	public abstract void init(AutoInstruction ai);
+	public abstract void update();
+	public abstract boolean isComplete();
+}
