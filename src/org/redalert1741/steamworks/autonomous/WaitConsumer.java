7f3bc475f52af4cb3204b34d4f3383a32f0ba602
randomtestfive <randomtestfive@gmail.com>
2017-03-18 13:20:51 -0400

    Basically do nothing (WaitConsumer)

---
+++ src/org/redalert1741/steamworks/autonomous/WaitConsumer.java
@@ -0,0 +1,10 @@
+package org.redalert1741.steamworks.autonomous;
+
+public class WaitConsumer extends TimedConsumer
+{
+	@Override
+	public void update()
+	{
+		//Wow nothing happens during a wait
+	}
+}
